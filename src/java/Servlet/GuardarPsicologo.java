ea49d3b2f6744e1fcc848daa07a78d3e8e732488
ThinKreations <jnapolesm1800@alumno.ipn.mx>
2021-06-17 13:54:27 -0500

    Bobes Ponja

--- src/java/Servlet/GuardarPsicologo.java
+++ src/java/Servlet/GuardarPsicologo.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Servlet;
 
 import Control.AccionesPaciente;
@@ -14,10 +19,9 @@ import javax.servlet.http.HttpSession;
 
 /**
  *
- * @author demon
+ * @author Ely
  */
-
-public class GuardarPaciente extends HttpServlet {
+public class GuardarPsicologo extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
